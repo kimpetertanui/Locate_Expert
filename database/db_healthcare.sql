432fafa0cd43392de381f69f3e46d865cfcbe7d7
kimpetertanui <kimelipeter526@gmail.com>
2020-05-05 19:33:43 +0300

    added database folder

---
+++ database/db_healthcare.sql
@@ -0,0 +1,183 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 05, 2020 at 11:29 AM
+-- Server version: 10.1.39-MariaDB
+-- PHP Version: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_healthcare`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `appId` int(3) NOT NULL,
+  `patientIc` bigint(12) NOT NULL,
+  `scheduleId` int(10) NOT NULL,
+  `appSymptom` varchar(100) NOT NULL,
+  `appComment` varchar(100) NOT NULL,
+  `status` varchar(10) NOT NULL DEFAULT 'process'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`appId`, `patientIc`, `scheduleId`, `appSymptom`, `appComment`, `status`) VALUES
+(87, 3185046, 45, 'Sneezing', 'Hello ,am sneezing alot', 'done');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `icDoctor` bigint(12) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `doctorId` int(3) NOT NULL,
+  `doctorFirstName` varchar(50) NOT NULL,
+  `doctorLastName` varchar(50) NOT NULL,
+  `doctorAddress` varchar(100) NOT NULL,
+  `doctorPhone` varchar(15) NOT NULL,
+  `doctorEmail` varchar(20) NOT NULL,
+  `doctorDOB` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`icDoctor`, `password`, `doctorId`, `doctorFirstName`, `doctorLastName`, `doctorAddress`, `doctorPhone`, `doctorEmail`, `doctorDOB`) VALUES
+(123456789, '123', 123, 'John', 'Mandela', 'Nairobi', '0717497458', 'doctor@gmail.com', '1990-04-10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctorschedule`
+--
+
+CREATE TABLE `doctorschedule` (
+  `scheduleId` int(11) NOT NULL,
+  `scheduleDate` date NOT NULL,
+  `scheduleDay` varchar(15) NOT NULL,
+  `startTime` time NOT NULL,
+  `endTime` time NOT NULL,
+  `bookAvail` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctorschedule`
+--
+
+INSERT INTO `doctorschedule` (`scheduleId`, `scheduleDate`, `scheduleDay`, `startTime`, `endTime`, `bookAvail`) VALUES
+(45, '2020-05-05', 'Tuesday', '14:30:00', '14:54:00', 'notavail'),
+(46, '2020-05-05', 'Tuesday', '17:30:00', '19:30:00', 'available');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `icPatient` bigint(12) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `patientFirstName` varchar(20) NOT NULL,
+  `patientLastName` varchar(20) NOT NULL,
+  `patientMaritialStatus` varchar(10) NOT NULL,
+  `patientDOB` date NOT NULL,
+  `patientGender` varchar(10) NOT NULL,
+  `patientAddress` varchar(100) NOT NULL,
+  `patientPhone` varchar(15) NOT NULL,
+  `patientEmail` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`icPatient`, `password`, `patientFirstName`, `patientLastName`, `patientMaritialStatus`, `patientDOB`, `patientGender`, `patientAddress`, `patientPhone`, `patientEmail`) VALUES
+(3185046, '12345', 'Peter', 'Kimeli', 'single', '1996-07-08', 'male', 'Nairobi', '708995320', 'kimelipeter20@gmail.com'),
+(30523052, '123', 'Monicah', 'Juma', 'single', '1992-05-17', 'femal', 'Shauri Moyo\r\nJogoo Road', '0722712103', 'monica@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`appId`),
+  ADD UNIQUE KEY `scheduleId_2` (`scheduleId`),
+  ADD KEY `patientIc` (`patientIc`),
+  ADD KEY `scheduleId` (`scheduleId`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`icDoctor`);
+
+--
+-- Indexes for table `doctorschedule`
+--
+ALTER TABLE `doctorschedule`
+  ADD PRIMARY KEY (`scheduleId`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`icPatient`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `appId` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
+
+--
+-- AUTO_INCREMENT for table `doctorschedule`
+--
+ALTER TABLE `doctorschedule`
+  MODIFY `scheduleId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `appointment_ibfk_4` FOREIGN KEY (`patientIc`) REFERENCES `patient` (`icPatient`),
+  ADD CONSTRAINT `appointment_ibfk_5` FOREIGN KEY (`scheduleId`) REFERENCES `doctorschedule` (`scheduleId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

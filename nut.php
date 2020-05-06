248502b919a5ff936dee591922f1d7319a75309a
kimpetertanui <kimelipeter526@gmail.com>
2020-05-06 05:51:20 +0300

    new updates

--- nut.php
+++ nut.php
@@ -1,33 +1,30 @@
 <?php
 include_once 'assets/conn/dbconnect.php';
-// include_once 'assets/conn/server.php';
 ?>
 
-
-<!-- login -->
-<!-- check session -->
 <?php
 session_start();
 // session_destroy();
-if (isset($_SESSION['patientSession']) != "") {
-header("Location: patient/patient.php");
+
+if (isset($_SESSION['nutSession']) != "") {
+header("Location: nutritionist/nut_details.php");
 }
 if (isset($_POST['login']))
 {
-$icPatient = mysqli_real_escape_string($con,$_POST['icPatient']);
+$id_no = mysqli_real_escape_string($con,$_POST['id_no']);
 $password  = mysqli_real_escape_string($con,$_POST['password']);
 
-$res = mysqli_query($con,"SELECT * FROM patient WHERE icPatient = '$icPatient'");
+$res = mysqli_query($con,"SELECT * FROM nutritionist WHERE id_no = '$id_no'");
 $row=mysqli_fetch_array($res,MYSQLI_ASSOC);
 if ($row['password'] == $password)
 {
-$_SESSION['patientSession'] = $row['icPatient'];
+$_SESSION['nutSession'] = $row['id_no'];
 ?>
 <script type="text/javascript">
 alert('Login Success');
 </script>
 <?php
-header("Location: patient/patient.php");
+header("Location: nutrition/nut_details.php");
 } else {
 ?>
 <script>
@@ -40,26 +37,24 @@ alert('wrong input ');
 <!-- register -->
 <?php
 if (isset($_POST['signup'])) {
-$patientFirstName = mysqli_real_escape_string($con,$_POST['patientFirstName']);
-$patientLastName  = mysqli_real_escape_string($con,$_POST['patientLastName']);
-$patientEmail     = mysqli_real_escape_string($con,$_POST['patientEmail']);
-$icPatient     = mysqli_real_escape_string($con,$_POST['icPatient']);
-$password         = mysqli_real_escape_string($con,$_POST['password']);
-$month            = mysqli_real_escape_string($con,$_POST['month']);
-$day              = mysqli_real_escape_string($con,$_POST['day']);
-$year             = mysqli_real_escape_string($con,$_POST['year']);
-$patientDOB       = $year . "-" . $month . "-" . $day;
-$patientGender = mysqli_real_escape_string($con,$_POST['patientGender']);
+$nutFirstName = mysqli_real_escape_string($con,$_POST['nutFirstName']);
+$nutLastName  = mysqli_real_escape_string($con,$_POST['nutLastName']);
+$id_no= mysqli_real_escape_string($con,$_POST['id_no']);
+$phone_no         = mysqli_real_escape_string($con,$_POST['phone_no']);
+$nut_name            = mysqli_real_escape_string($con,$_POST['nut_name']);
+$location           = mysqli_real_escape_string($con,$_POST['location']);
+$password              = mysqli_real_escape_string($con,$_POST['password']);
 //INSERT
-$query = " INSERT INTO patient (  icPatient, password, patientFirstName, patientLastName,  patientDOB, patientGender,   patientEmail )
-VALUES ( '$icPatient', '$password', '$patientFirstName', '$patientLastName', '$patientDOB', '$patientGender', '$patientEmail' ) ";
+$query = " INSERT INTO nutritionist (  fname, lname, id_no, phone_no,  nut_name, location,   password )
+VALUES ( '$nutFirstName', '$nutLastName', '$id_no', '$phone_no', '$nut_name', '$location', '$password' ) ";
 $result = mysqli_query($con, $query);
 // echo $result;
 if( $result )
 {
+    header("Location: nutrition/nut_details.php");
 ?>
 <script type="text/javascript">
-alert('Register success. Please Login to make an appointment.');
+alert('Register success. Please Login to  to add your products and find client requests.');
 </script>
 <?php
 }
@@ -176,123 +171,25 @@ alert('User already registered. Please try again');
                                         <h4>It's free and always will be.</h4>
                                         <div class="row">
                                             <div class="col-xs-6 col-md-6">
-                                                <input type="text" name="patientFirstName" value="" class="form-control input-lg" placeholder="First Name" required />
+                                                <input type="text" name="nutFirstName" value="" class="form-control input-lg" placeholder="First Name" required />
                                             </div>
                                             <div class="col-xs-6 col-md-6">
-                                                <input type="text" name="patientLastName" value="" class="form-control input-lg" placeholder="Last Name" required />
+                                                <input type="text" name="nutLastName" value="" class="form-control input-lg" placeholder="Last Name" required />
                                             </div>
                                         </div>
                                         
-                                        <input type="text" name="patientEmail" value="" class="form-control input-lg" placeholder="Your Email"  required/>
-                                        <input type="number" name="icPatient" value="" class="form-control input-lg" placeholder="Your IC Number"  required/>
+                                        <input type="text" name="id_no" value="" class="form-control input-lg" placeholder="Your ID Number"  required/>
+                                        <input type="text" name="phone_no" value="" class="form-control input-lg" placeholder="Your Phone Number"  required/>
                                         
+                                        
+                                        <input type="text" name="nut_name" value="" class="form-control input-lg" placeholder="Nutritionist Name"  required/>
+
+                                        <input type="text" name="location" value="" class="form-control input-lg" placeholder="Location Address"  required/>
                                         
                                         <input type="password" name="password" value="" class="form-control input-lg" placeholder="Password"  required/>
                                         
-                                        <input type="password" name="confirm_password" value="" class="form-control input-lg" placeholder="Confirm Password"  required/>
-                                        <label>Birth Date</label>
-                                        <div class="row">
-                                            
-                                            <div class="col-xs-4 col-md-4">
-                                                <select name="month" class = "form-control input-lg" required>
-                                                    <option value="">Month</option>
-                                                    <option value="01">Jan</option>
-                                                    <option value="02">Feb</option>
-                                                    <option value="03">Mar</option>
-                                                    <option value="04">Apr</option>
-                                                    <option value="05">May</option>
-                                                    <option value="06">Jun</option>
-                                                    <option value="07">Jul</option>
-                                                    <option value="08">Aug</option>
-                                                    <option value="09">Sep</option>
-                                                    <option value="10">Oct</option>
-                                                    <option value="11">Nov</option>
-                                                    <option value="12">Dec</option>
-                                                </select>
-                                            </div>
-                                            <div class="col-xs-4 col-md-4">
-                                                <select name="day" class = "form-control input-lg" required>
-                                                    <option value="">Day</option>
-                                                    <option value="01">1</option>
-                                                    <option value="02">2</option>
-                                                    <option value="03">3</option>
-                                                    <option value="04">4</option>
-                                                    <option value="05">5</option>
-                                                    <option value="06">6</option>
-                                                    <option value="07">7</option>
-                                                    <option value="08">8</option>
-                                                    <option value="09">9</option>
-                                                    <option value="10">10</option>
-                                                    <option value="11">11</option>
-                                                    <option value="12">12</option>
-                                                    <option value="13">13</option>
-                                                    <option value="14">14</option>
-                                                    <option value="15">15</option>
-                                                    <option value="16">16</option>
-                                                    <option value="17">17</option>
-                                                    <option value="18">18</option>
-                                                    <option value="19">19</option>
-                                                    <option value="20">20</option>
-                                                    <option value="21">21</option>
-                                                    <option value="22">22</option>
-                                                    <option value="23">23</option>
-                                                    <option value="24">24</option>
-                                                    <option value="25">25</option>
-                                                    <option value="26">26</option>
-                                                    <option value="27">27</option>
-                                                    <option value="28">28</option>
-                                                    <option value="29">29</option>
-                                                    <option value="30">30</option>
-                                                    <option value="31">31</option>
-                                                </select>
-                                            </div>
-                                            <div class="col-xs-4 col-md-4">
-                                                <select name="year" class = "form-control input-lg" required>
-                                                    <option value="">Year</option>
-                                                    
-                                                    <option value="1981">1981</option>
-                                                    <option value="1982">1982</option>
-                                                    <option value="1983">1983</option>
-                                                    <option value="1984">1984</option>
-                                                    <option value="1985">1985</option>
-                                                    <option value="1986">1986</option>
-                                                    <option value="1987">1987</option>
-                                                    <option value="1988">1988</option>
-                                                    <option value="1989">1989</option>
-                                                    <option value="1990">1990</option>
-                                                    <option value="1991">1991</option>
-                                                    <option value="1992">1992</option>
-                                                    <option value="1993">1993</option>
-                                                    <option value="1994">1994</option>
-                                                    <option value="1995">1995</option>
-                                                    <option value="1996">1996</option>
-                                                    <option value="1997">1997</option>
-                                                    <option value="1998">1998</option>
-                                                    <option value="1999">1999</option>
-                                                    <option value="2000">2000</option>
-                                                    <option value="2001">2001</option>
-                                                    <option value="2002">2002</option>
-                                                    <option value="2003">2003</option>
-                                                    <option value="2004">2004</option>
-                                                    <option value="2005">2005</option>
-                                                    <option value="2006">2006</option>
-                                                    <option value="2007">2007</option>
-                                                    <option value="2008">2008</option>
-                                                    <option value="2009">2009</option>
-                                                    <option value="2010">2010</option>
-                                                    <option value="2011">2011</option>
-                                                    <option value="2012">2012</option>
-                                                    <option value="2013">2013</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <label>Gender : </label>
-                                        <label class="radio-inline">
-                                            <input type="radio" name="patientGender" value="male" required/>Male
-                                        </label>
-                                        <label class="radio-inline" >
-                                            <input type="radio" name="patientGender" value="female" required/>Female
-                                        </label>
+                                        
+                                    
                                         <br />
                                         <span class="help-block">By clicking Create my account, you agree to our Terms and that you have read our Data Use Policy, including our Cookie Use.</span>
                                         
@@ -315,7 +212,10 @@ alert('User already registered. Please try again');
                 <div class="row">
                     <div class="col-md-5">
                         <h2>This is still Under construction!</h2>
-                        <p>This is Nutritionist's page.Please wait. </p>
+                        <p>This is Nutritionist's page.Please wait. 
+                            <?php 
+                            $_SESSION['userName'] = 'Root' ?>
+              </p>
                      
                        
                         <!-- date textbox end -->
@@ -341,20 +241,7 @@ alert('User already registered. Please try again');
                 <!-- /.row -->
             </div>
         </section>
-        <!-- first section end -->
-
-        
-        <!-- second section start -->
-        
-        <!-- second section end -->
-        <!-- third section start -->
-        
-        <!-- third section end -->
-        <!-- forth sections start -->
-       
-
-        <!-- forth section end -->
-        <!-- footer start -->
+    
         <div class="copyright-bar bg-black">
             <div class="container">
                 <p class="pull-left small">© Locate Expert</p>
--- pharmacist.php
+++ pharmacist.php
@@ -80,7 +80,7 @@ alert('User already registered. Please try again');
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
         <title>Locate Expert</title>
         <!-- Bootstrap -->
         <!-- <link href="assets/css/bootstrap.min.css" rel="stylesheet"> -->
